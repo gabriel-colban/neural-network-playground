61db65f0d5323f3ba6898257fb41959ab6321808
Ron <itsronmydudes@gmail.com>
2025-05-14 21:33:52 +0200

    Added result evaluation, some changes to network init

--- main.py
+++ main.py
@@ -25,9 +25,11 @@ def sigmoid(x):
 
 class Neural_network():
     def __init__(self):
-        self.x = None
-        self.y = None
-        self.z = None
+
+        # Randomly selected coordinates
+        self.x = random.randint(-20, 20)
+        self.y = random.randint(-20, 20)
+        self.z = random.randint(-20, 20)
 
         self.layers = []
 
@@ -43,22 +45,34 @@ class Neural_network():
         curr_values = self.values
         next_values = []
 
-        for neuron in self.layers[self.progress]:
-            output = neuron.compute_value(curr_values)
-            next_values.append(output)
+        if self.progress < len(self.layers):
+            for neuron in self.layers[self.progress]:
+                output = neuron.compute_value(curr_values)
+                next_values.append(output)
+        elif self.progress == len(self.layers):
+            final_value = 0
+            for n in curr_values:
+                final_value += n
+            final_value = sigmoid(final_value)
+            next_values.append(final_value)
+            print("Layers completed")
+        else:
+            next_values = self.values
+            print("Layers completed")
+
+        self.values = next_values
+        self.progress += 1
 
 
-class Neuron(Neural_network):
-    def __init__(self):
-        super.__init__()
-        self.w = None
-        self.b = None
-        self.value = None
+class Neuron():
+    def __init__(self, num_inputs):
+        self.w = np.random.rand(num_inputs)
+        self.b = random.random()
 
     def compute_value(self, values:list):
         # computes a value based on input values
         curr_value = 0
-
+        print(values)
         for n in range(len(values)):
             curr_value += values[n] * self.w[n] * self.b
 
@@ -67,12 +81,15 @@ class Neuron(Neural_network):
         return curr_value
 
 
+def is_inside_room(x,y,z):
+
+
+    # Room:  3 < x < 10, 2 < y < 8, 6 < z < 12.
+
+    return x > 3 and x < 10 and y > 2 and y < 8 and z > 6 and z < 12
+
 def start_network():
     neural_network = Neural_network()
-
-    x = random.randint(-20, 20)
-    y = random.randint(-20, 20)
-    z = random.randint(-20, 20)
 
     layers = 1
     neurons = 4
@@ -82,16 +99,33 @@ def start_network():
         # so we need one weight for each neuron
         layer = []
         for j in range(neurons):
-            neuron = Neuron()
-
-            weights = []
-
-            for n in range(1,3):
-                weights.append(random.random())
-
-            neuron.w = weights
-            neuron.b = random.random()
+            if i == 0:
+                # If its the first input neuron takes as many inputs as the network has
+                num_inputs = len(neural_network.values)
+            else:
+                # If its deeper, neuron takes as many inputs as there are neurons before it.
+                num_inputs = len(neural_network.layers[i-1])
+            neuron = Neuron(num_inputs)
 
             layer.append(neuron)
 
         neural_network.layers.append(layer)
+
+    # run through layers
+
+    for x in range(len(neural_network.layers)+1):
+        neural_network.compute()
+    print(f"Coordinates: {neural_network.x}, {neural_network.y}, {neural_network.z}")
+    print("Final value: ", neural_network.values[0])
+    prediction = neural_network.values[0] > 0.5
+    if prediction:
+        print("Network predicted room is inside")
+    else:
+        print("Network predicted room is outside")
+    is_inside = is_inside_room(neural_network.x,neural_network.y,neural_network.z)
+    if is_inside:
+        print("Random coordinates is inside room")
+    else:
+        print("Random coordinates is outside room")
+
+start_network()
